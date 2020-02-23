5287effb1c7c8fed87bb0fec7535c30c276e09e0
Martin Witczak <martin.witczak.extern@ergo.digital>
2020-02-23 10:47:01 +0000

    Added migrations for mysql and example auth using passport.js

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,54 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const passport = require('passport');
+
+const { UniqueTokenStrategy } = require('passport-unique-token');
+
+const strategyOptions = {
+  failOnMissing: true
+};
+
+passport.use(new UniqueTokenStrategy(strategyOptions,
+  (token, done) => {
+  return done(null, {'test': 'test'});
+    /*User.findOne({
+      uniqueToken: token,
+      expireToken: { $gt: Date.now() }
+    }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });*/
+  }));
+
+function authenticate(req, res, next) {
+  passport.authenticate('token', (err, user, info) => {
+    console.log('user', user);
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      res.status(401).json({ message: 'Incorrect token credentials' });
+    }
+
+    req.user = user;
+    next();
+  })(req, res, next);
+}
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/test', authenticate, (req, res) => {
+  // User authenticated and can be found in req.user
+  res.send(200);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
