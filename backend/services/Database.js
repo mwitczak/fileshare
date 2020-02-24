8b3ff61aec5b7b3f5a3fd1cce7c797b2b5433bbe
Martin Witczak <martin.witczak.extern@ergo.digital>
2020-02-24 16:05:05 +0000

    Added env variables for backend mysql connection

--- backend/services/Database.js
+++ backend/services/Database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const database = new Sequelize('instashare', 'root', 'example', {
+const database = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
   host: 'db',
   dialect: 'mysql',
 });
